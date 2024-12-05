871ee74c28d6c76b6b185ace0b6cd9f8abf7fede
janpc01 <janpc01@gmail.com>
2024-12-05 15:07:13 -0700

    Profile component which will later display Card List

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home/home.component';
 // Authentication
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
+import { ProfileComponent } from './authentication/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: '' }, // Redirect unknown routes to home
 ];
