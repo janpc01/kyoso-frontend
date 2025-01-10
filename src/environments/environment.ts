707be7b448517b355592aba07056065d6596e4e2
janpc01 <janpc01@gmail.com>
2025-01-10 15:45:39 -0700

    Kyoso stripe keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiUrl: 'https://kyoso-backend-dhheg8akajdre6ce.canadacentral-01.azurewebsites.net/api',
-    stripePublishableKey: 'pk_test_51QMlBeKDLvEmLtClMuR2LhouEhOyclSDcEx2K5yLsOsnb3lECXj7dvYoHrF506DXBxEOtmIscmiAMxqnevwXLvGv00y79Lv8ze',
+    stripePublishableKey: 'pk_test_51QfO3OG4xZKRTgNlZ0m6B2Y1hwhHWokcKaLvs3SzKvELASaLjK0c8XVTdnCz3ogfxJLrq20kv3mPgDTR7LmKv3u200Pwph1FDs',
 };
   
